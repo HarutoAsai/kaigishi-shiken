eb0c978f2fbccf667064f3fbb88ec816136530a8
HarutoAsai <225707355+HarutoAsai@users.noreply.github.com>
2025-08-30 16:34:56 +0900

    feat: topic result (no accuracy label) & level progress cards; purge '正答率' labels

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "707501926"
+    serviceWorkerVersion: "2990794207"
   }
 });
