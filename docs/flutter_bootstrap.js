a33f3c41416b8c252c60d242f81eda3e0b6f578b
HarutoAsai <225707355+HarutoAsai@users.noreply.github.com>
2025-08-30 13:28:32 +0900

    fix(app-builder): inject ForceScrollShell builder into actual App widget(s) 20250830132737

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3320966132"
+    serviceWorkerVersion: "3932999739"
   }
 });
