c65523c6c5b578582bc972f3e6cd71ad2b90fdc5
HarutoAsai <225707355+HarutoAsai@users.noreply.github.com>
2025-09-03 22:08:03 +0900

    chore: kill SW and bust cache

---
+++ docs/flutter_service_worker.js
@@ -0,0 +1,12 @@
+﻿self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("activate", e => {
+  e.waitUntil((async () => {
+    try {
+      await self.registration.unregister();
+      const cs = await self.clients.matchAll({type:"window"});
+      for (const c of cs) { c.navigate(c.url); }
+    } catch (err) { /* ignore */ }
+  })());
+});
+// no-op
+self.addEventListener("fetch", e => {});
