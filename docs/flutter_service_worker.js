3ccb2f83cacdaeee2d94f8a0a7cebc50cd732846
HarutoAsai <225707355+HarutoAsai@users.noreply.github.com>
2025-09-03 14:20:17 +0900

    chore: kill old service worker on GH Pages

---
+++ docs/flutter_service_worker.js
@@ -0,0 +1,12 @@
+﻿self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("activate", e => {
+  e.waitUntil((async () => {
+    try {
+      await self.registration.unregister();
+      const clientsArr = await self.clients.matchAll({type:"window"});
+      for (const c of clientsArr) { c.navigate(c.url); }
+    } catch (err) {}
+  })());
+});
+// 何もしない（ネットに任せる）
+self.addEventListener("fetch", e => {});
